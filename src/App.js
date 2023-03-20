8fddf6492fdae18942d50183185956d70d59b2d3
SaritSopitvetmontree <sarit.sopitvetmontree@yahoo.com>
2023-03-20 21:19:16 +0900

    Create component files

--- src/App.js
+++ src/App.js
@@ -40,7 +40,31 @@ const App = () => {
                     </div>
 
                     <div>
-                        
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path='/' element='ECommerce' />
+                            <Route path='/ecommerce' element='ECommerce' />
+
+                            {/* Pages */}
+                            <Route path='/orders' element='Orders' />
+                            <Route path='/employees' element='Employees' />
+                            <Route path='/customers' element='Customers' />
+
+                            {/* Apps */}
+                            <Route path='/kanban' element='Kanban' />
+                            <Route path='/editor' element='Editor' />
+                            <Route path='/calendar' element='Calendar' />
+                            <Route path='/color-picker' element='Color-picker' />
+
+                            {/* Charts */}
+                            <Route path='/line' element='Line' />
+                            <Route path='/area' element='Area' />
+                            <Route path='/bar' element='Bar' />
+                            <Route path='/pie' element='Pie' />
+                            <Route path='/financial' element='Financial' />
+                            <Route path='/color-mapping' element='Color-mapping' />
+                            <Route path='/stacked' element='Stacked' />
+                        </Routes>
                     </div>
                 
                 </div>
